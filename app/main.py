00a060f834c9809f2e61f1ce21a985e8fd66ae9d
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-05-08 19:16:53 +0200

    cors

--- app/main.py
+++ app/main.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.api import router
 from app.core.config import settings
 
 app = FastAPI(debug=settings.DEBUG, openapi_url="/api/docs.json", docs_url="/api")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex="http://localhost:\d+",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(
     router,
     prefix="/api",
 )
-
 
 app.openapi_schema = get_openapi(
     title="Conduit API",
