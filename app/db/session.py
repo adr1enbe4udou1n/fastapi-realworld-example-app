a1acc172a2a6d9f0aba34dd46b12625a88393df4
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-01-28 13:38:03 +0100

    up to sql alch 2.0

--- app/db/session.py
+++ app/db/session.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings, settingsReadOnly
 
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 
 engineReadOnly = create_engine(settingsReadOnly.DATABASE_RO_URL, pool_pre_ping=True)
-SessionLocalRo = sessionmaker(autocommit=False, autoflush=False, bind=engineReadOnly)
+SessionLocalRo = sessionmaker(
+    autocommit=False, autoflush=False, bind=engineReadOnly, future=True
+)
--- tests/conftest.py
+++ tests/conftest.py
@@ -19,7 +19,9 @@ from app.models.article import Article  # noqa: E402
 from app.models.user import User  # noqa: E402
 
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, future=True
+)
 
 Base.metadata.create_all(bind=engine)
 
