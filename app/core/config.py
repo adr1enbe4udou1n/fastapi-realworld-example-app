6f77c939904c765f1bc0613941ace457d4287604
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-03-26 14:31:49 +0200

    fix pytest db env

--- app/core/config.py
+++ app/core/config.py
@@ -13,11 +13,11 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str = secrets.token_urlsafe(32)
     JWT_EXPIRE: int = 60 * 24 * 8
 
-    DB_HOST: str
-    DB_PORT: int
-    DB_DATABASE: str
-    DB_USERNAME: str
-    DB_PASSWORD: str
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5433
+    DB_DATABASE: str = "main"
+    DB_USERNAME: str = "main"
+    DB_PASSWORD: str = "main"
     DATABASE_URL: Optional[PostgresDsn] = None
     DATABASE_RO_URL: Optional[PostgresDsn] = None
 
@@ -41,12 +41,12 @@ class Settings(BaseSettings):
 
 
 class SettingsReadOnly(BaseSettings):
-    DB_HOST: str
+    DB_HOST: str = "localhost"
     DB_RO_HOST: Optional[str] = None
-    DB_PORT: int
-    DB_DATABASE: str
-    DB_USERNAME: str
-    DB_PASSWORD: str
+    DB_PORT: int = 5433
+    DB_DATABASE: str = "main"
+    DB_USERNAME: str = "main"
+    DB_PASSWORD: str = "main"
     DATABASE_RO_URL: Optional[PostgresDsn] = None
 
     @validator("DATABASE_RO_URL", pre=True)
@@ -68,18 +68,5 @@ class SettingsReadOnly(BaseSettings):
         )
 
 
-settings = Settings(
-    DB_HOST=os.getenv("DB_HOST") or "localhost",
-    DB_PORT=int(os.getenv("DB_PORT") or "5433"),
-    DB_DATABASE=os.getenv("DB_DATABASE") or "main",
-    DB_USERNAME=os.getenv("DB_USERNAME") or "main",
-    DB_PASSWORD=os.getenv("DB_PASSWORD") or "main",
-)
-settingsReadOnly = SettingsReadOnly(
-    DB_HOST=os.getenv("DB_HOST") or "localhost",
-    DB_RO_HOST=os.getenv("DB_RO_HOST") or "localhost",
-    DB_PORT=int(os.getenv("DB_PORT") or "5433"),
-    DB_DATABASE=os.getenv("DB_DATABASE") or "main",
-    DB_USERNAME=os.getenv("DB_USERNAME") or "main",
-    DB_PASSWORD=os.getenv("DB_PASSWORD") or "main",
-)
+settings = Settings()
+settingsReadOnly = SettingsReadOnly()
